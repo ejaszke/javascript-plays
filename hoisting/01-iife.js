118d501d00a13404ea7ac218ee00c8afb8c97afc
Edward Jaszke <edward.jaszke@gmail.com>
2016-10-04 09:40:04 +0200

    added block building plays

---
+++ hoisting/01-iife.js
@@ -0,0 +1,16 @@
+/**
+ * Answer for 01.js with IIFE
+ */
+var funcs = [];
+
+for (var i = 0; i < 10; i++) {
+    funcs.push((function(value) {
+        return function() {
+            console.log(value);
+        }
+    }(i)));
+}
+
+funcs.forEach(function(func) {
+    func();
+});
