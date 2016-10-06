77727d23ad0fafdec2badf71107195f5e9f659ba
Grzegorz Batko <grzegorz@softbat.pl>
2016-10-06 09:13:14 +0200

    label change

--- strings/strings.js
+++ strings/strings.js
@@ -8,8 +8,6 @@ console.log(text.charCodeAt(1));
 
 console.log(String.fromCodePoint(134071));  
 
-Jakie wyniki ?
-
 function codePointLength(text) {
     var result = text.match(/[\s\S]/gu);
     return result ? result.length : 0;
@@ -19,20 +17,18 @@ console.log(text.codePointAt(0));
 console.log(codePointLength("abc"));    
 console.log(codePointLength("𠮷bc"));  
 
-Jakie wyniki?
 // ----------------
 let message1 = `Multiline\nstring`,
     message2 = String.raw`Multiline\nstring`;
 
 console.log(message1);          
 console.log(message2);  
-
-Jaki wynik?        
+      
 // ----------------
 
 var str = "Karol ma karty";
 var n = str.search(/kar/i); 
 console.log(n);
 
-Jaki wynik?
+What are the results of all console.log ?
 
